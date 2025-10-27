6c4ba4e6db54daf01ce7c2f0125001d8828177cc
menahem_erlich <erlichmenahem@gmail.com>
2025-10-27 10:12:06 +0200

    I finished function apply_guess and it works.

--- hangman/game.py
+++ hangman/game.py
@@ -1,17 +1,25 @@
 
 def init_state(secret: str, max_tries: int) -> dict:
-    game_info = {}
-    game_info["secret"] = secret
-    game_info["display"] = "_" * len(secret)
-    game_info["guessed"] = []
-    game_info["wrong_guesses"] = None
-    game_info["max_tries"] = max_tries
-    return  game_info
+    state = {}
+    state["secret"] = secret
+    state["display"] = "_" * len(secret)
+    state["guessed"] = []
+    state["wrong_guesses"] = 0
+    state["max_tries"] = max_tries
+    return  state
 
 def validate_guess(ch: str, guesses: list[str]) -> tuple[bool, str]:
     if len(ch) == 1 and ch.isalpha() and ch not in guesses:
         return True, ch
     return False, ch
+
+def apply_guess(state: dict, ch: str) -> bool:
+    if ch in state["secret"]:
+        return False
+    state["wrong_guesses"] += 1
+    state["max_tries"] -= 1
+    return True
+
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from data.words_list import words
 from hangman.words import choose_secret_word
-from hangman.game import init_state, validate_guess
+from hangman.game import init_state, validate_guess, apply_guess
 
 
 
 ch = input("Enter a ch: ")
-game_info = init_state(choose_secret_word(words), max_tries=5)
-print(validate_guess(ch, game_info["guessed"]))
+state = init_state(choose_secret_word(words), max_tries=5)
+validate_guess(ch, state["guessed"])
+print(state)
+print(apply_guess(state, ch))
+print(state)
